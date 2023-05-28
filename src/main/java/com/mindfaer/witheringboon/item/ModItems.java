fc0549a552ab0fe627e5efe38cb3911d69c55eae
Ash Sylvan <75921990+MindFaer@users.noreply.github.com>
2023-05-28 17:31:58 -0400

    Added ten new gems, and melded geode

--- src/main/java/com/mindfaer/witheringboon/item/ModItems.java
+++ src/main/java/com/mindfaer/witheringboon/item/ModItems.java
@@ -15,8 +15,28 @@ public class ModItems {
 
     public static final RegistryObject<Item> HOARFROSTHEAD = ITEMS.register("hoarfrosthead",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE)));
-
-
+    public static final RegistryObject<Item> MELDEDGEODE = ITEMS.register("meldedgeode",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> ROSEQUARTZ = ITEMS.register("rosequartz",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> DUMORTIERITEQUARTZ = ITEMS.register("dumortieritequartz",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> PRASE = ITEMS.register("prase",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> KYANITE = ITEMS.register("kyanite",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> JASPER = ITEMS.register("jasper",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> TANZANITE = ITEMS.register("tanzanite",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> CHALCANTHITE = ITEMS.register("chalcanthite",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> PERIDOT = ITEMS.register("peridot",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> MALACHITE = ITEMS.register("malachite",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
