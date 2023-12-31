3778d3acbce26e23eb018397da5d0aacefcca531
Ash Sylvan <75921990+MindFaer@users.noreply.github.com>
2023-12-31 05:31:43 -0500

    Small cleanup

--- src/main/java/com/mindfaer/witheringboon/witheringboonmain.java
+++ src/main/java/com/mindfaer/witheringboon/witheringboonmain.java
@@ -15,7 +15,6 @@ import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(witheringboonmain.MOD_ID)
 public class witheringboonmain
 {
@@ -35,12 +34,10 @@ public class witheringboonmain
     private void commonSetup(final FMLCommonSetupEvent event)
     {}
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {}
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents
     {
